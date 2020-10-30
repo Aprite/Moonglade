8d1d17fa1747a312deec0380f246b08a13ee7082
shaoxiaoxu <shaoxiaoxu@autobio.com.cn>
2020-10-30 19:46:22 +0800

    feat: add localaccount

--- src/Moonglade.Data/Migrations/20201030114008_addlocalaccount.Designer.cs
+++ src/Moonglade.Data/Migrations/20201030114008_addlocalaccount.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Moonglade.Data;
 
 namespace Moonglade.Data.Migrations
 {
     [DbContext(typeof(BlogDbContext))]
-    partial class BlogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201030114008_addlocalaccount")]
+    partial class addlocalaccount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Moonglade.Data/Migrations/20201030114008_addlocalaccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Moonglade.Data.Migrations
+{
+    public partial class addlocalaccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LocalAccount",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Username = table.Column<string>(maxLength: 32, nullable: true),
+                    PasswordHash = table.Column<string>(maxLength: 64, nullable: true),
+                    LastLoginTimeUtc = table.Column<DateTime>(type: "datetime", nullable: true),
+                    LastLoginIp = table.Column<string>(maxLength: 64, nullable: true),
+                    CreateOnUtc = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocalAccount", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LocalAccount");
+        }
+    }
+}
